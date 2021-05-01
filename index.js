e6063fac3938ad9a0fe004572f1e0df33818c764
Grigoriy <cerberus414@gmail.com>
2021-05-01 16:45:13 -0500

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+var test = "hello";
+const {a, b, ...rest} = {a: 1, b: 2, c:3};
+
+var foo = function() {
+    console.log("start");
+    glb = "global";
+};
+
+var bar = function() {
+    console.log(glb);
+    console.log(test);
+    console.log(a);
+    console.log(b);
+    console.log(rest);
+};
+
+foo();
+bar();
